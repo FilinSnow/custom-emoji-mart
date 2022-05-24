34b561d2f23bff590af8611151a8e7bf3f4fdfe8
vladislavproskurkin <vproskurin.exceedteam@gmail.com>
2022-05-24 13:30:49 +0300

    test fix

--- packages/emoji-mart/src/components/HOCs/PureInlineComponent.js
+++ packages/emoji-mart/src/components/HOCs/PureInlineComponent.js
@@ -1,17 +1,17 @@
 import { PureComponent } from 'preact/compat'
 
 export default class PureInlineComponent extends PureComponent {
-  shouldComponentUpdate(nextProps) {
-    for (let k in nextProps) {
-      if (k == 'children') continue
+  // shouldComponentUpdate(nextProps) {
+  //   for (let k in nextProps) {
+  //     if (k == 'children') continue
 
-      if (nextProps[k] != this.props[k]) {
-        return true
-      }
-    }
+  //     if (nextProps[k] != this.props[k]) {
+  //       return true
+  //     }
+  //   }
 
-    return false
-  }
+  //   return false
+  // }
 
   render() {
     return this.props.children
